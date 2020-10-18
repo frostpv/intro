11392dd6b9c196df4510efd5c4ed27de2d8422fe
Laszlo Petechuk <frostpv@gmail.com>
2020-10-18 22:56:35 +0300

    delete redundant annotation

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -3,14 +3,12 @@ package service;
 import dao.UserDao;
 import java.util.List;
 import models.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
 
-    @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
