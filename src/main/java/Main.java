005a32d559b46dc6ea1cf70b3631fbdf5087f7c8
Laszlo Petechuk <frostpv@gmail.com>
2020-10-18 19:43:12 +0300

    fix main, add logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import config.AppConfig;
 import models.User;
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import service.UserService;
 
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context
                 = new AnnotationConfigApplicationContext(AppConfig.class);
@@ -16,5 +19,6 @@ public class Main {
         ira.setEmail("irina@gmail.com");
         ira.setName("Irina");
         userService.add(ira);
+        logger.info("Get list users: " + userService.listUsers());
     }
 }
