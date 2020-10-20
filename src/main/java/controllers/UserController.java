800652382be7d223cc81f67c0ae5f458e994541d
Laszlo Petechuk <frostpv@gmail.com>
2020-10-20 19:07:54 +0300

    remove redundant controllers

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -2,13 +2,22 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@ResponseBody
+@RestController
 public class UserController {
     @GetMapping(value = "/user/{userId}")
     public String getUser() {
         return "";
+    }
+
+    @GetMapping(value = "/user/inject")
+    public String injectUser() {
+        return "test inject";
+    }
+    @GetMapping(value = "/user/")
+    public String getAllUser() {
+        return "users-test";
     }
 }
