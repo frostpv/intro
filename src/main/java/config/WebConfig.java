c464b120081f3d41e6394006c5527bb6a896868e
Laszlo Petechuk <frostpv@gmail.com>
2020-10-20 19:56:22 +0300

    remove redundant method from config

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,23 +1,11 @@
 package config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan(basePackages = {"controllers"})
 @EnableWebMvc
 public class WebConfig {
-
-    @Bean
-    public InternalResourceViewResolver resolver(){
-        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setViewClass(JstlView.class);
-        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
-        internalResourceViewResolver.setSuffix(".jsp");
-        return internalResourceViewResolver;
-    }
 }
