8b93b3f3e7b8381541064b5335f8dad1b6dc310a
Laszlo Petechuk <frostpv@gmail.com>
2020-10-20 18:21:32 +0300

    fixed pom.xml

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -2,8 +2,10 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = {"controllers"})
+@EnableWebMvc
 public class WebConfig {
 }
